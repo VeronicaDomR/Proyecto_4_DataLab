60c6f4befae4424b6c6bc0f2f6ad57586f95c22b
VeronicaDomR <99091553+VeronicaDomR@users.noreply.github.com>
2024-08-14 16:56:49 -0600

    sql

--- SQL/variables_dummies_y conteo.sql
+++ SQL/variables_dummies_y conteo.sql
@@ -4,16 +4,16 @@ WITH word_counts AS (
     cleaned_review_content,
 
     -- Conteo de palabras positivas en review_title
-    ARRAY_LENGTH(REGEXP_EXTRACT_ALL(review_title, r'\b(new|best|quick|great|high|light|easy|right|Good|full|many|able|fine|amazing|bright|hot|decent|handy|nice|satisfied|useful|smooth|comfortable|fast|super|extraordinary|wonderful)\b')) AS positive_count_title,
+    ARRAY_LENGTH(REGEXP_EXTRACT_ALL(review_title, r'\b(new|best|quick|great|high|light|easy|right|Good|full|many|able|fine|amazing|bright|hot|decent|handy|nice|satisfied|useful|smooth|comfortable|fast|super|extraordinary|wonderful|like|well|better|worth|original|recommend|easily|support|ok)\b')) AS positive_count_title,
 
     -- Conteo de palabras negativas en review_title
-    ARRAY_LENGTH(REGEXP_EXTRACT_ALL(review_title, r'\b(difficult|worst|jammed|infamous|black|bad|flat|less|little|small|long|rough|past|few|other)\b')) AS negative_count_title,
+    ARRAY_LENGTH(REGEXP_EXTRACT_ALL(review_title, r'\b(difficult|worst|jammed|infamous|black|bad|flat|less|little|small|long|rough|past|few|other|problem|low|bad|hard|stopped|poor|difficult|lag|damaged|lower|disappointed|loose|missing|cut|damage|defective)\b')) AS negative_count_title,
 
     -- Conteo de palabras positivas en cleaned_review_content
-    ARRAY_LENGTH(REGEXP_EXTRACT_ALL(cleaned_review_content, r'\b(new|best|quick|great|high|light|easy|right|Good|full|many|able|fine|amazing|bright|hot|decent|handy|nice|satisfied|useful|smooth|comfortable|fast|super|extraordinary|wonderful)\b')) AS positive_count_content,
+    ARRAY_LENGTH(REGEXP_EXTRACT_ALL(cleaned_review_content, r'\b(new|best|quick|great|high|light|easy|right|Good|full|many|able|fine|amazing|bright|hot|decent|handy|nice|satisfied|useful|smooth|comfortable|fast|super|extraordinary|wonderful|like|well|better|worth|original|recommend|easily|support|ok)\b')) AS positive_count_content,
 
     -- Conteo de palabras negativas en cleaned_review_content
-    ARRAY_LENGTH(REGEXP_EXTRACT_ALL(cleaned_review_content, r'\b(difficult|worst|jammed|infamous|black|bad|flat|less|little|small|long|rough|past|few|other)\b')) AS negative_count_content
+    ARRAY_LENGTH(REGEXP_EXTRACT_ALL(cleaned_review_content, r'\b(difficult|worst|jammed|infamous|black|bad|flat|less|little|small|long|rough|past|few|other|problem|low|bad|hard|stopped|poor|difficult|lag|damaged|lower|disappointed|loose|missing|cut|damage|defective)\b')) AS negative_count_content
 
   FROM
     `proyecto4datalab.amazon.datset`
@@ -25,31 +25,33 @@ SELECT
 
   -- Variable dummy para palabras positivas en review_title
   CASE 
-    WHEN REGEXP_CONTAINS(review_title, r'\b(new|best|quick|great|high|light|easy|right|Good|full|many|able|fine|amazing|bright|hot|decent|handy|nice|satisfied|useful|smooth|comfortable|fast|super|extraordinary|wonderful)\b') 
+    WHEN REGEXP_CONTAINS(review_title, r'\b(new|best|quick|great|high|light|easy|right|Good|full|many|able|fine|amazing|bright|hot|decent|handy|nice|satisfied|useful|smooth|comfortable|fast|super|extraordinary|wonderful|like|well|better|worth|original|recommend|easily|support|ok)\b') 
     THEN 1 
     ELSE 0 
-  END AS has_positive_title,
+  END AS dummie_positive_title,
 
   -- Variable dummy para palabras negativas en review_title
   CASE 
-    WHEN REGEXP_CONTAINS(review_title, r'\b(difficult|worst|jammed|infamous|black|bad|flat|less|little|small|long|rough|past|few|other)\b') 
+    WHEN REGEXP_CONTAINS(review_title, r'\b(difficult|worst|jammed|infamous|black|bad|flat|less|little|small|long|rough|past|few|other|problem|low|bad|hard|stopped|poor|difficult|lag|damaged|lower|disappointed|loose|missing|cut|damage|defective)\b') 
     THEN 1 
     ELSE 0 
-  END AS has_negative_title,
+  END AS dummie_negative_title,
 
   -- Variable dummy para palabras positivas en cleaned_review_content
   CASE 
-    WHEN REGEXP_CONTAINS(cleaned_review_content, r'\b(new|best|quick|great|high|light|easy|right|Good|full|many|able|fine|amazing|bright|hot|decent|handy|nice|satisfied|useful|smooth|comfortable|fast|super|extraordinary|wonderful)\b') 
+    WHEN REGEXP_CONTAINS(cleaned_review_content, r'\b(new|best|quick|great|high|light|easy|right|Good|full|many|able|fine|amazing|bright|hot|decent|handy|nice|satisfied|useful|smooth|comfortable|fast|super|extraordinary|wonderful|like|well|better|worth|original|recommend|easily|support|ok)\b') 
+
+    
     THEN 1 
     ELSE 0 
-  END AS has_positive_content,
+  END AS dummie_positive_content,
 
   -- Variable dummy para palabras negativas en cleaned_review_content
   CASE 
-    WHEN REGEXP_CONTAINS(cleaned_review_content, r'\b(difficult|worst|jammed|infamous|black|bad|flat|less|little|small|long|rough|past|few|other)\b') 
+    WHEN REGEXP_CONTAINS(cleaned_review_content, r'\b(difficult|worst|jammed|infamous|black|bad|flat|less|little|small|long|rough|past|few|other|problem|low|bad|hard|stopped|poor|difficult|lag|damaged|lower|disappointed|loose|missing|cut|damage|defective)\b') 
     THEN 1 
     ELSE 0 
-  END AS has_negative_content,
+  END AS dummie_negative_content,
 
   positive_count_title,
   negative_count_title,
